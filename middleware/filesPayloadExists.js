a028db3e070e2e16fb013f4a192501aae15fbc39
DonwiwatM <tonameto@gmail.com>
2024-01-20 17:05:11 +0700

    initial files. Project kick off good luck

---
+++ middleware/filesPayloadExists.js
@@ -0,0 +1,7 @@
+const filesPayPayloadExists = (req, res, next) => {
+    if (!req.files) return res.status(400).json({ status: "error", message: "Missing files"})
+
+    next()
+}
+
+module.exports = filesPayPayloadExists
